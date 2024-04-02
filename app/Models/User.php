02e010c53ca9bdf1682e36528cf54f78b25bf52b
ahmadnurfadilah <ahmadnurfadilah22@gmail.com>
2024-04-02 18:23:20 +0700

    init

---
+++ app/Models/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable, SoftDeletes;
+
+    protected $fillable = [
+        'telegram_id',
+        'first_name',
+        'last_name',
+        'username',
+        'photo_url',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
